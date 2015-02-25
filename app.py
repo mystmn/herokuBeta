dbeafd10878603a29201428eccc2f3bc221424e2
Paul Cameron <pcameron5@gmail.com>
2015-02-25 07:24:22 -0500

    worked in production after disable. Enabling 500 redirect

--- app.py
+++ app.py
@@ -87,9 +87,9 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
-#@app.errorhandler(500)
-#def internal_error(error):
-#    return render_template('500.html'), 500
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
