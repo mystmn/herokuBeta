e001a3a7e50fc03273376c83a37d0ae7fedac5e2
Paul Cameron <pcameron5@gmail.com>
2015-02-26 08:06:41 -0500

    Stil won't send and email and redirect *sigh

--- app.py
+++ app.py
@@ -65,7 +65,6 @@ def contact():
     if request.method == 'POST':
         if form.validate():
             print "Does this print fine?"
-            '''
             message=sendgrid.Mail()
             message.add_to("ufr.server@gmail.com")
             message.set_from("ufr.server@gmail.com")
@@ -73,7 +72,6 @@ def contact():
             mes_con="<p>Person of Interest: %s</p><p>Email: %s</p><p>Subject: %s</p><p>Message: %s</p>" % (form.name.data, form.email.data, form.subject.data, form.message.data)
             message.set_html(mes_con)
             sg.send(message)
-            '''
             return render_template('contact.html', form=form, title=title, posted_redirect=True)
         else:
             return render_template('contact.html', form=form, title=title, posted_redirect=False)
